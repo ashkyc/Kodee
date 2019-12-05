510059a3a8ac8341976071c63767471301b088c0
Ash M <46741511+ashkyc@users.noreply.github.com>
2019-12-05 15:46:06 -0800

    controller files without api keys

---
+++ midTier/Kodee/Controllers/AddressApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Kodee.Model.Domain;
+using Kodee.Model.Requests;
+using Kodee.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Kodee.Controllers
+{
+    [Route("api/addresses")]
+    [ApiController]
+    public class AddressApiController : ControllerBase
+    {
+        private IAddressService _addressService = null;
+
+        public AddressApiController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpPost]
+        public ActionResult<int> Insert(AddressAddRequest model)
+        {
+            try
+            {
+                int id = _addressService.Insert(model);
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        
+    }
+}
